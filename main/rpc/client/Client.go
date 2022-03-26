c82834e7c97d11878029b96db7734bf35aed591d
yuhangbin <yuhangbinccc@gmail.com>
2022-03-27 01:21:12 +0800

    feat: RPC C/S demo

--- main/rpc/client/Client.go
+++ main/rpc/client/Client.go
@@ -1,6 +1,26 @@
 package main
 
-const serverAddress = "127.0.0.1"
-func main() {
+import (
+	"example/main/rpc/core"
+	"fmt"
+	"log"
+	"net/rpc"
+)
 
+const serverAddress = "127.0.0.1"
+
+func main() {
+	client, err := rpc.DialHTTP("tcp", serverAddress+":1234")
+	if err != nil {
+		log.Fatal("dialing:", err)
+	}
+
+	// Synchronous call
+	args := &core.Args{7, 8}
+	var reply int
+	err = client.Call("Arith.Multiply", args, &reply)
+	if err != nil {
+		log.Fatal("arith error:", err)
+	}
+	fmt.Printf("Arith: %d*%d=%d", args.A, args.B, reply)
 }
