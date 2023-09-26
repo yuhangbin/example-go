dd16019c033d695c9d520f5c8589160fa2139143
yuhangbin <yuhangbinccc@gmail.com>
2023-09-27 00:36:20 +0800

    feat: medium leetcode

---
+++ leetcode/container-with-most-water.go
@@ -0,0 +1,31 @@
+package leetcode
+
+func maxArea(height []int) int {
+	head, tail := 0, len(height)-1
+
+	max := 0
+
+	for head < tail {
+		max = Max(max, (tail-head)*Min(height[tail], height[head]))
+		if height[tail] < height[head] {
+			tail = tail - 1
+		} else {
+			head = head + 1
+		}
+	}
+	return max
+}
+
+func Min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func Max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
